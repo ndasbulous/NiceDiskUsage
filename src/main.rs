277a7999227cd3a81a8691a9dce8f9ced3f44da1
Emanuel Anggit <ndasbulous@gmail.com>
2025-04-27 22:07:08 +0700

    Display folder size in human readable format

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,20 @@ fn main() {
     let results = get_folder_size::get_folder_size("d:\\");
 
     for entry in &results {
-        println!("{} {}", entry.0, entry.1);
+        print!("{} ", entry.0);
+        match entry.1 {
+            0..1024 => {
+                println!("{} B", entry.1);
+            }
+            1024..1048576 => {
+                println!("{} KB", entry.1 / 1024);
+            }
+            1048576..1073741824 => {
+                println!("{} MB", entry.1 / 1024 / 1024);
+            }
+            _ => {
+                println!("{} GB", entry.1 / 1024 / 1024 / 1024);
+            }
+        }
     }
 }
